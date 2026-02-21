7fbea0aa1602951c06cc40e44c5ece38ac657c1a
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-02-21 21:58:56 +0530

    Add files via upload

---
+++ src/main/java/com/Bank/app/model/User.java
@@ -0,0 +1,33 @@
+package com.Bank.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,  unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users-roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role> roles;
+}
