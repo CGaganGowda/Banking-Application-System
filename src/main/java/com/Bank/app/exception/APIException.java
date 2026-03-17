f60a173eb55f6d3aabaf74766414a1f622385c7e
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-03-17 22:15:20 +0530

    Create APIException.java

---
+++ src/main/java/com/Bank/app/exception/APIException.java
@@ -0,0 +1,10 @@
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public class TodoApiException extends RuntimeException {
+    private HttpStatus status;
+    private String message;
+}
