1b514b147eaab49a2133500fc8764b8b5386a987
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-04-09 23:17:54 +0530

    Refactor AccountController to remove unused imports

--- src/main/java/com/Bank/app/controller/AccountController.java
+++ src/main/java/com/Bank/app/controller/AccountController.java
@@ -4,7 +4,6 @@ import com.Bank.app.dto.AccountDto;
 import com.Bank.app.dto.TransactionDto;
 import com.Bank.app.dto.TransferFundsDto;
 import com.Bank.app.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +16,7 @@ import java.util.Map;
 @RequestMapping("api/accounts")
 public class AccountController {
 
-    @Autowired
     private AccountService accountService;
-
-
 
     @PostMapping("")
     public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto) {
