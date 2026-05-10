b8e6c510c17ae5b25a80aae478ce04e3b9b7854f
Gagan C Gowda <95483586+CGaganGowda@users.noreply.github.com>
2026-05-10 22:09:58 +0530

    Add OpenAPI definition to AppApplication
    Added OpenAPI definition for the Banking Management System.

--- src/main/java/com/Bank/app/AppApplication.java
+++ src/main/java/com/Bank/app/AppApplication.java
@@ -3,6 +3,24 @@ package com.Bank.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking Management System",
+				description = "Banking service with simple and secure API's",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Gagan",
+						email = "gagancgowda971@gmail.com"
+				),
+				license = @License(
+						name = "Apache 1.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Banking Documentation",
+				url = "#"
+		)
+)
 @SpringBootApplication
 public class AppApplication {
 
