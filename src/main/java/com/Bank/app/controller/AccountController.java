175a70d80c7ded769a1cbd32464b34e7c16f5353
Gagan C Gowda <95483586+CGaganGowda@users.noreply.github.com>
2026-05-11 20:41:24 +0530

    Enhance AccountController with OpenAPI annotations
    Added OpenAPI annotations for better API documentation and updated response codes.

--- src/main/java/com/Bank/app/controller/AccountController.java
+++ src/main/java/com/Bank/app/controller/AccountController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+
+@Tag(
+        name = "REST API for Banking System",
+        description = "Banking Application API documentation"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/accounts")
@@ -19,48 +24,110 @@ public class AccountController {
 
     private AccountService accountService;
 
+    @Operation(
+            summary = "CREATE API",
+            description = "Creating a customer"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "CUSTOMER CREATED"
+    )
     @PostMapping("")
     public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto) {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "GET CUSTOMER ON ID",
+            description = "Provides the details of customer with the given ID."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Customer with given ID is returned."
+    )
     @GetMapping("{id}")
     public ResponseEntity<AccountDto> getAccountId(@PathVariable Long id){
         return new ResponseEntity<>(accountService.getAccountById(id), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "DEPOSIT AMOUNT",
+            description = "Deposits the amount in to the customer's account"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Amount is deposited in to customer's account and balance is displayed."
+    )
     @PutMapping("{id}/deposit")
     public ResponseEntity<AccountDto> depositMoney(@PathVariable Long id,@RequestBody Map<String,Double> map){
         double amount = map.get("amount");
         return new ResponseEntity<>(accountService.deposit(id,amount),HttpStatus.OK);
     }
 
+        @Operation(
+            summary = "WITHDRAW AMOUNT",
+            description = "Withdraws the amount from  the customer's account"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Amount is withdrawn from the customer's account and balance is displayed."
+    )
     @PutMapping("{id}/withdraw")
     public ResponseEntity<AccountDto> withdrawMoney(@PathVariable Long id,@RequestBody Map<String,Double> map){
         double amount = map.get("amount");
         return new ResponseEntity<>(accountService.withdraw(id,amount),HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "GET ALL CUSTOMERS",
+            description = "Provides the details of all the customers."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Customers are returned."
+    )
     @GetMapping()
     public ResponseEntity<List<AccountDto>> getAllAccounts(){
         return new ResponseEntity<>(accountService.getAllAccounts(),HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "DELETE Customer with given ID",
+            description = "Deletes the customer account."
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "Customer is deleted."
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteAccount(@PathVariable Long id){
         accountService.deleteAccountById(id);
-        return new ResponseEntity<>("Account Deleted with id : "+id, HttpStatus.OK);
+        return new ResponseEntity<>("Account Deleted with id : "+id, HttpStatus.NO_CONTENT);
     }
 
+    @Operation(
+            summary = "TRANSFER FUNDS",
+            description = "Transfer funds between the valid source and destination customers."
+    @ApiResponse(
+            responseCode = "200",
+            description = "Funds transferred successfully."
+    )
     @PostMapping("transfer")
     public ResponseEntity<String> transferAmount(@RequestBody TransferFundsDto transferFundsDto){
         accountService.transferFunds(transferFundsDto);
         return new ResponseEntity<>("Amount is successfully transferred",HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "GET ALL TRANSACTIONS OF A CUSTOMER",
+            description = "Provides all the transaction details for a customer with given ID."
+    @ApiResponse(
+            responseCode = "200",
+            description = "Transactions shown successfully"
+    )
     @GetMapping("{id}/transactions")
-    public List<TransactionDto> getAllTransactionsByAccountId(@PathVariable Long id){
-        return accountService.getAllTransactions(id);
+    public ResponseEntity<List<TransactionDto>> getAllTransactionsByAccountId(@PathVariable Long id){
+        return new ResponseEntity<>(accountService.getAllTransactions(id),HttpStatus.OK);
     }
 
 
